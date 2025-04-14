9373853c7b8a994097fe0624d4952afe4f2629e0
muhirwa1mj <muhirwaj1@gmail.com>
2025-04-15 00:39:44 +0200

    Save progress before pulling remote branch

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,7 @@
 <x-layout>
 
     <h1 class="title">Latest Posts</h1>
+    
 
 {{-- <img src="{{ asset('storage/posts_images/public/storage/posts_images/b2y4Zavc1wQg7PRzmVlygOH2WNn69qyTKtX7IMOC.jpg')}}" alt=""> --}}
 
--- routes/web.php
+++ routes/web.php
@@ -3,52 +3,36 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\ResetPasswordController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/','posts.index')->name('home');
+// Home route
+Route::view('/', 'posts.index')->name('home');
+Route::redirect('/', 'posts');
 
-Route::redirect('/','posts');
+// User posts route
+Route::get('/{user}/posts', [DashboardController::class, 'userPosts'])->name('posts.user');
 
-//User posts route
-Route::get('/{user}/posts',[DashboardController::class,'userPosts'])->name('posts.user');
+// Post resource routes
+Route::resource('posts', PostController::class);
 
-
-Route::resource('posts',PostController::class);
-
-//Routes for authenticated users
-Route::middleware('auth')->group(function(){
-
-
-Route::get('/dashboard',[DashboardController::class,'index'])->middleware('verified')->name('dashboard');
-
-
-Route::post('/logout',[AuthController::class,'logout'])->name('logout');
-
-
-//Email verification Notice route 
-
-
-Route::get('/email/verify', [AuthController::class,'verifyNotice'])->name('verification.notice');
-
-//Email Verification Handler Notice 
-
-Route::get('/email/verify/{id}/{hash}', [AuthController::class,'verifyEmail'])->middleware(['signed'])->name('verification.verify');
-
-//Resending the email verification route 
-
-Route::post('/email/verification-notification', [AuthController::class,'verifyHandler'])->middleware(['throttle:6,1'])->name('verification.send');
-
+// Routes for authenticated users
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
 
+// Routes for guests (not logged in)
+Route::middleware('guest')->group(function () {
+    Route::view('/register', 'auth.register')->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
 
-Route::middleware('guest')->group(function() {
+    Route::view('/login', 'auth.login')->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
 
-Route::view('/register','auth.register')->name('register'); 
-
-Route::post('/register', [App\Http\Controllers\AuthController::class,'register']);
-
-Route::view('/login','auth.login')->name('login'); 
-
-Route::post('/login', [App\Http\Controllers\AuthController::class,'login']);
-
+    // Reset Password Routes
+    Route::view('/forgot-password', 'auth.forgot-password')->name('password.request');
+    Route::post('/forgot-password', [ResetPasswordController::class, 'passwordEmail']);
+    Route::get('/reset-password/{token}', [ResetPasswordController::class, 'passwordReset'])->name('password.reset');
+    Route::post('/reset-password', [ResetPasswordController::class, 'passwordUpdate'])->name('password.update');
 });
